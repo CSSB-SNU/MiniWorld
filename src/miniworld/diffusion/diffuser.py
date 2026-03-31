173d9cf39144381e606cd4661217abc78e12aef0
SanggeunParrk <psk6950@snu.ac.kr>
2026-04-01 00:25:11 +0900

    add: Decoupled EDM

--- src/miniworld/diffusion/diffuser.py
+++ src/miniworld/diffusion/diffuser.py
@@ -45,6 +45,7 @@ class Diffuser(ABC):
     ) -> None:
         self.config = config
         self.scheduler = scheduler
+        self.clear_buffer()
         self._set_seed(config.seed)
 
     def _set_seed(self, seed: int) -> None:
