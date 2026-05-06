20c674eca1d57c815e8a0305b40666c983ebf46f
bsoohyuncd <jb051@snu.ac.kr>
2026-05-06 21:04:17 +0900

    fix: import error and config

--- scripts/run_af3like_entire_ep.py
+++ scripts/run_af3like_entire_ep.py
@@ -26,8 +26,8 @@ from miniworld.configs import (
     TokenizerConfig,
 )
 from miniworld.data.dataloader.dataloader import BioMolData
-from miniworld.models import DefaultClient_rev as Client
-from miniworld.models.af3_like import Model_rev as Model
+from miniworld.models import DefaultClient as Client
+from miniworld.models.af3_like import Model as Model
 from miniworld.utils import get_step_decay_scheduler_with_warmup
 
 torch.set_float32_matmul_precision("medium")
